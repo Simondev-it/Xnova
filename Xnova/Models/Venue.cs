fe181f9deeacc2fa7ee531b3dcb7a6b148b126ff
Simondev-it <Duongntse180440@fpt.edu.vn>
2025-06-20 11:14:49 +0700

    add entity

--- Xnova/Models/Venue.cs
+++ Xnova/Models/Venue.cs
@@ -16,6 +16,9 @@ public partial class Venue
     public int? Status { get; set; }
 
     public int? UserId { get; set; }
+    public string ? Longitude { get; set; } 
+
+    public string? Latitude { get; set; }
 
     public virtual ICollection<Field> Fields { get; set; } = new List<Field>();
 
